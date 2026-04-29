2b814e1fc359bf6e741c354c967f2de3b6f02ec6
Jesse Taavola <jesse@taavola.dev>
2026-04-29 12:17:30 -0500

    add custom posthog api host (reverse proxy)

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -54,6 +54,7 @@ function RootDocument({ children }: { children: React.ReactNode }) {
 				<PostHogProvider
 					apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_PROJECT_TOKEN || ""}
 					options={{
+						api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
 						ui_host: "https://us.posthog.com",
 						defaults: "2026-01-30",
 						capture_exceptions: true,
